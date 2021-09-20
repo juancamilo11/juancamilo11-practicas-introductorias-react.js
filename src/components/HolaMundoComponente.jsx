5125b66648f88ac6e175744abe9036210d1c8428
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-20 09:36:42 -0500

    Creacion de componente basado en clases

---
+++ src/components/HolaMundoComponente.jsx
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+
+class HolaMundoComponent extends Component{
+    render(){
+        return <h2>Hola soy un componente</h2>
+    }
+}
+
+export default HolaMundoComponent;
